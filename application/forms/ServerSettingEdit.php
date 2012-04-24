03c8e7544ea22ee491a247a0696436d56bcb8eb1
Nico Nägele <nixalio@gmail.com>
2012-04-24 22:59:14 +0200

    fixed bug

--- application/forms/ServerSettingEdit.php
+++ application/forms/ServerSettingEdit.php
@@ -6,10 +6,16 @@
  */
 class Application_Form_ServerSettingEdit extends \SAP\Form
 {
+	/**
+	 * @var Application_Model_ServerSetting
+	 */
+	protected $_model;
+
 	public function init()
 	{
 		$defaultValueField = new Zend_Form_Element_Text('default_value');
-		$defaultValueField->setLabel('Default value');
+		$defaultValueField->setLabel('Default value')
+			->setValue($this->_model->getDefaultValue());
 
 		$submitButton = new Zend_Form_Element_Submit('edit');
 
