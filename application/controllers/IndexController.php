69b2ef9687a75bacd976be18dc10f4a257f04f31
Nico Nägele <nixalio@gmail.com>
2012-05-24 23:05:56 +0200

    layout changes

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,12 @@
 <?php
-
 class IndexController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
-    public function indexAction()
-    {
-        // action body
-    }
-
-
+	public function indexAction()
+	{
+		$this->view->assign(array(
+			'streams' => array(),
+			'tickets' => array(),
+		));
+	}
 }
 
